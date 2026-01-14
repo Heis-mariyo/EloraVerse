f059aca2f52b3bedd37a12c6150d0fa361fe0eee
Heis-mariyo <yoyoizunobi@gmail.com>
2026-01-14 16:38:15 +0100

    header fix

--- src/app/components/bookcard/bookcard.component.ts
+++ src/app/components/bookcard/bookcard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bookcard',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './bookcard.component.css'
 })
 export class BookcardComponent {
-
+  @Input() book: any;
 }
